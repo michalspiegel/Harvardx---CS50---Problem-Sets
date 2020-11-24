aa26c1504d42489c07b8d0029226af8b7e721e30
michalspiegel <70861221+michalspiegel@users.noreply.github.com>
2020-11-24 14:27:17 +0100

    Update credit.py

--- credit.py
+++ credit.py
@@ -5,37 +5,47 @@ import sys
 
 
 def main():
+    # Number input
     card_number = input("Your card number:")
     lenght = len(card_number)
-
+    # Check if it is only composed of numbers
     if (card_number.isdigit()) == False:
         print("INVALID")
         sys.exit(1)
-        
-    if (12 < int(card_number) > 17) == False:
+    # Check if it has the right lenght    
+    if len(card_number) not in [13, 15, 16]:
         print("INVALID")
         sys.exit(1)
-        
+    # Check if the luhn's algorithm "approves" the number    
     if luhn(card_number) == False:
         print("INVALID")
         sys.exit(1)
 
-    
+
+
+    # First two digits of the card number
     first_digits = card_number[0] + card_number[1]
+    # Check for AMEX specification - standarts
     if lenght == 15 and first_digits in ["34", "37"]:
         print("AMEX")
+    # MASTERCARD specification    
     elif lenght == 16 and first_digits in ["51","52","53","54","55"]:
         print("MASTERCARD")
+    # VISA specification    
     elif (lenght == 13 or lenght == 16) and card_number[0] == "4":
         print("VISA")
+        
     else:
         print("INVALID")
+
+
 
 
 def luhn(card_number):
     card_number = list(map(int, card_number))
     sum1 = 0
     sum2 = 0
+    
     
     # Calculate sum2 - every other digit - starting from the second-to-last digit 
     for i in range(len(card_number) - 2, -1, -2):
@@ -46,14 +56,17 @@ def luhn(card_number):
         else:
             sum1 += card_number[i] * 2
             
+            
     # Calculate sum2 - every other digit - starting from the last digit
     for i in range(len(card_number) - 1, -1, -2):
         sum2 += card_number[i]
+
 
     # Calculate final outcome
     if (sum1 + sum2) % 10 == 0:
         return True
     else:
         return False
+
 
 main()
