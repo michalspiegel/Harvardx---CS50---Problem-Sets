d7a096c23957e8b9c619ffedb3a78980d17402e9
michalspiegel <70861221+michalspiegel@users.noreply.github.com>
2020-12-08 10:45:50 +0100

    Update dna.py

--- dna.py
+++ dna.py
@@ -1,7 +1,7 @@
 # Takes as an input a string - a DNA sequence
 # Outputs whether someone in the dictionary matches this DNA sequence in STRs
-from sys import argv
-from sys import exit
+
+from sys import argv, exit
 import csv
 
 
@@ -32,37 +32,60 @@ with open(argv[1], "r") as file:
             dna_id_number += row[i]
         id_database.update({name : dna_id_number})
 
+
 # Opening and reading .txt file with dna sequence
 with open(argv[2], "r") as file:
     dna_sequence = file.read()
 
 
+
 # Checking for max consecutive repeats of an STR in a dna sequence
 for STR in STRs:
+
     # Maximum of consecutive repeats of the given STR in the given dna sequence
-    max_repeats = 1
-    
-    while True:
+    consecutive_repeats = []
 
-        if STR*max_repeats in dna_sequence:
-            counter += 1
-            continue
+    # For every char in dna_sequence
+    for i in range(len(dna_sequence)):
 
+        tempMax = 0
+        l = len(STR)
+
+        # Slice array into subarrays with length l and see if it equals to STR
+        if dna_sequence[i: i + l] == STR:
+            tempMax = 1
+
+            #If yes, see, if it repeats
+            while dna_sequence[i + l: i + 2*l] == dna_sequence[i: i+ l]:
+
+                #If yes add "one repeat" to tempMax and advance in list
+                tempMax += 1
+                i += l
+
+            consecutive_repeats.append(tempMax)
+
+
+    # If the list is not empty
+    if consecutive_repeats:
         # Creating a special dna ID
-        dna_id = dna_id + str(counter - 1)
-        break
+        dna_id = dna_id + str(max(consecutive_repeats))
 
 
-# Final searching of identical dna_id
-match = False
 
+
+# Final searching for identical dna_id
 for person in id_database.keys():
+
     if id_database[person] == dna_id:
         print(person)
-        match = True
+        exit(0)
 
-if match == False:
-    print("No match")
+print("No match")
+
+
+
+
+
 
 
 
